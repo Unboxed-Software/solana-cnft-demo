81631ec9b3e71ebd5cf04a5efb5674d90443b8db
John <johnzhengyuliu@gmail.com>
2023-05-31 23:07:32 -0500

    refactor

--- src/test.ts
+++ src/test.ts
@@ -2,18 +2,15 @@ import { initializeKeypair } from "./initializeKeypair"
 import {
   PublicKey,
   Connection,
-  clusterApiUrl,
   Keypair,
   sendAndConfirmTransaction,
   Transaction,
-  TransactionInstruction,
   AccountMeta,
 } from "@solana/web3.js"
 import {
   SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
   ValidDepthSizePair,
   createAllocTreeIx,
-  getConcurrentMerkleTreeAccountSize,
   SPL_NOOP_PROGRAM_ID,
   ConcurrentMerkleTreeAccount,
 } from "@solana/spl-account-compression"
@@ -26,10 +23,7 @@ import {
   createMintToCollectionV1Instruction,
   createTransferInstruction,
 } from "@metaplex-foundation/mpl-bubblegum"
-import {
-  PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID,
-  CreateMetadataAccountArgsV3,
-} from "@metaplex-foundation/mpl-token-metadata"
+import { PROGRAM_ID as TOKEN_METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata"
 import {
   CreateCompressedNftOutput,
   keypairIdentity,
@@ -37,34 +31,35 @@ import {
 } from "@metaplex-foundation/js"
 import fetch from "node-fetch"
 
-describe("Test", () => {
-  const connection = new Connection(clusterApiUrl("devnet"), "confirmed")
+describe("Compressed NFTs", () => {
+  const rpcUrl = process.env.RPC_URL
+  const connection = new Connection(rpcUrl, "confirmed")
   const treeKeypair = Keypair.generate()
 
-  let user: Keypair
+  let payer: Keypair
   let collectionNft: CreateCompressedNftOutput
 
   let cnfts: any[] = []
 
   before(async () => {
-    user = await initializeKeypair(connection)
+    payer = await initializeKeypair(connection)
 
-    const metaplex = new Metaplex(connection).use(keypairIdentity(user))
+    const metaplex = new Metaplex(connection).use(keypairIdentity(payer))
 
     collectionNft = await metaplex.nfts().create({
       uri: "https://madlads.s3.us-west-2.amazonaws.com/json/2382.json",
-      name: "JPEG",
+      name: "Collection NFT",
       sellerFeeBasisPoints: 100,
-      updateAuthority: user,
-      mintAuthority: user,
+      updateAuthority: payer,
+      mintAuthority: payer,
       tokenStandard: 0,
-      symbol: "JPEG",
+      symbol: "Collection",
       isMutable: true,
       isCollection: true,
     })
   })
 
-  it("Merkle Tree Creation", async () => {
+  it("Create Tree", async () => {
     const maxDepthSizePair: ValidDepthSizePair = {
       maxDepth: 3,
       maxBufferSize: 8,
@@ -72,28 +67,75 @@ describe("Test", () => {
 
     const canopyDepth = maxDepthSizePair.maxDepth - 5
 
-    const tree = await createTree(
+    const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
+      [treeKeypair.publicKey.toBuffer()],
+      BUBBLEGUM_PROGRAM_ID
+    )
+
+    const allocTreeIx = await createAllocTreeIx(
       connection,
-      user,
-      treeKeypair,
+      treeKeypair.publicKey,
+      payer.publicKey,
       maxDepthSizePair,
       canopyDepth
     )
+
+    const createTreeIx = createCreateTreeInstruction(
+      {
+        payer: payer.publicKey,
+        treeCreator: payer.publicKey,
+        treeAuthority,
+        merkleTree: treeKeypair.publicKey,
+        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+        logWrapper: SPL_NOOP_PROGRAM_ID,
+      },
+      {
+        maxBufferSize: maxDepthSizePair.maxBufferSize,
+        maxDepth: maxDepthSizePair.maxDepth,
+        public: false,
+      },
+      BUBBLEGUM_PROGRAM_ID
+    )
+
+    try {
+      const tx = new Transaction().add(allocTreeIx, createTreeIx)
+      tx.feePayer = payer.publicKey
+
+      const txSignature = await sendAndConfirmTransaction(
+        connection,
+        tx,
+        [treeKeypair, payer],
+        {
+          commitment: "confirmed",
+          skipPreflight: true,
+        }
+      )
+
+      console.log(
+        `https://explorer.solana.com/tx/${txSignature}?cluster=devnet`
+      )
+    } catch (err: any) {
+      console.error("\nFailed to create merkle tree:", err)
+
+      throw err
+    }
   })
 
-  it("Mint Compressed NFT", async () => {
-    // use similar parameters as used in the main() function
+  it("Mint Compressed NFT to Tree", async () => {
+    // Define constants
+    const treeAddress = treeKeypair.publicKey
+    const collectionDetails = {
+      mint: new PublicKey(collectionNft.mintAddress),
+      metadata: new PublicKey(collectionNft.metadataAddress),
+      masterEditionAccount: new PublicKey(collectionNft.masterEditionAddress),
+    }
+
+    // Compressed NFT Metadata
     const compressedNFTMetadata: MetadataArgs = {
-      name: "JPEG",
-      symbol: "JPEG",
+      name: "CNFT",
+      symbol: "CNFT",
       uri: "https://madlads.s3.us-west-2.amazonaws.com/json/8566.json",
-      creators: [
-        {
-          address: user.publicKey,
-          verified: false,
-          share: 100,
-        },
-      ],
+      creators: [{ address: payer.publicKey, verified: false, share: 100 }],
       editionNonce: 0,
       uses: null,
       collection: null,
@@ -104,39 +146,63 @@ describe("Test", () => {
       tokenStandard: TokenStandard.NonFungible,
     }
 
-    // assuming you have a valid treeAddress, collectionMint, collectionMetadata, collectionMasterEditionAccount
-    const treeAddress = treeKeypair.publicKey
-    const collectionMint = new PublicKey(collectionNft.mintAddress)
-    const collectionMetadata = new PublicKey(collectionNft.metadataAddress)
-    const collectionMasterEditionAccount = new PublicKey(
-      collectionNft.masterEditionAddress
+    const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
+      [treeAddress.toBuffer()],
+      BUBBLEGUM_PROGRAM_ID
     )
 
-    await mintCompressedNFT(
-      connection,
-      user,
-      treeAddress,
-      collectionMint,
-      collectionMetadata,
-      collectionMasterEditionAccount,
-      compressedNFTMetadata,
-      user.publicKey
+    const [bubblegumSigner, _bump2] = PublicKey.findProgramAddressSync(
+      [Buffer.from("collection_cpi", "utf8")],
+      BUBBLEGUM_PROGRAM_ID
     )
 
-    await mintCompressedNFT(
-      connection,
-      user,
-      treeAddress,
-      collectionMint,
-      collectionMetadata,
-      collectionMasterEditionAccount,
-      compressedNFTMetadata,
-      user.publicKey
+    // Create the mint instruction.
+    const mintIx = createMintToCollectionV1Instruction(
+      {
+        payer: payer.publicKey,
+        merkleTree: treeAddress,
+        treeAuthority,
+        treeDelegate: payer.publicKey,
+        leafOwner: payer.publicKey,
+        leafDelegate: payer.publicKey,
+        collectionAuthority: payer.publicKey,
+        collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
+        collectionMint: collectionDetails.mint,
+        collectionMetadata: collectionDetails.metadata,
+        editionAccount: collectionDetails.masterEditionAccount,
+        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
+        logWrapper: SPL_NOOP_PROGRAM_ID,
+        bubblegumSigner,
+        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
+      },
+      {
+        metadataArgs: Object.assign(compressedNFTMetadata, {
+          collection: { key: collectionDetails.mint, verified: false },
+        }),
+      }
     )
+
+    try {
+      const tx = new Transaction().add(mintIx)
+      tx.feePayer = payer.publicKey
+      const txSignature = await sendAndConfirmTransaction(
+        connection,
+        tx,
+        [payer],
+        { commitment: "confirmed", skipPreflight: true }
+      )
+
+      console.log(
+        `https://explorer.solana.com/tx/${txSignature}?cluster=devnet`
+      )
+    } catch (err) {
+      console.error("\nFailed to mint compressed NFT:", err)
+      throw err
+    }
   })
 
-  it("getAssetsByOwner", async () => {
-    const response = await fetch(process.env.RPC_URL!, {
+  it("Fetch NFTs and Filter by Collection", async () => {
+    const response = await fetch(rpcUrl, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -146,101 +212,92 @@ describe("Test", () => {
         id: "my-id",
         method: "getAssetsByOwner",
         params: {
-          ownerAddress: user.publicKey.toBase58(),
+          ownerAddress: payer.publicKey.toBase58(),
           page: 1, // Starts at 1
           limit: 1000,
         },
       }),
     })
+
     const { result } = await response.json()
-    // console.log(JSON.stringify(result, null, 2))
-    // console.log(result)
-    const filteredItems = result.items
-      ?.filter((item) =>
-        item.grouping.some(
-          (group) => group.group_value === collectionNft.mintAddress.toBase58()
-        )
-      )
-      .map((item) => {
-        // display some info about the item
-        console.log("itemId:", item.id)
-        console.log("ownership:", item.ownership)
-        console.log("compression:", item.compression)
 
-        // return an object with the info about the item
-        // return item.id
-        return item
-      })
-    console.log("Assets by Group: ", filteredItems)
-
-    cnfts = filteredItems
+    // Filter items to only include those from a specific collection.
+    const mintAddress = collectionNft.mintAddress.toBase58()
+    cnfts = result.items?.filter((item) =>
+      item.grouping.some((group) => group.group_value === mintAddress)
+    )
   })
 
-  it("transfer cnft", async () => {
+  it("Transfer Compressed NFT", async () => {
+    // Define constants
     const cnft = cnfts[0]
-    console.log("cnft", cnft)
+    const compression = cnft.compression
+    const treePublicKey = treeKeypair.publicKey
 
-    const response = await fetch(process.env.RPC_URL, {
+    // Fetch the asset proof.
+    const response = await fetch(rpcUrl, {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
         jsonrpc: "2.0",
         id: "my-id",
         method: "getAssetProof",
-        params: {
-          id: cnft.id, // compressed nft asset id
-        },
+        params: { id: cnft.id },
       }),
     })
-    const { result } = await response.json()
-    console.log("Assets Proof: ", result)
 
+    const { result } = await response.json()
+    // console.log("Assets Proof: ", result)
+
+    // Fetch the tree account.
     const treeAccount = await ConcurrentMerkleTreeAccount.fromAccountAddress(
       connection,
-      treeKeypair.publicKey
+      treePublicKey
     )
 
-    const newLeafOwner = Keypair.generate().publicKey
-    const leafOwner = new PublicKey(cnft.ownership.owner)
-    const leafDelegate = !!cnft.ownership?.delegate
+    const ownerPublicKey = new PublicKey(cnft.ownership.owner)
+    const delegatePublicKey = cnft.ownership.delegate
       ? new PublicKey(cnft.ownership.delegate)
-      : leafOwner
+      : ownerPublicKey
 
+    // Define keys for the new and current leaf owners.
+    // const newLeafOwner = Keypair.generate().publicKey
+    const leafOwner = ownerPublicKey
+    const leafDelegate = delegatePublicKey
+
+    // Get the tree authority and canopy depth.
     const treeAuthority = treeAccount.getAuthority()
-    const canopyDepth = treeAccount.getCanopyDepth()
+    const canopyDepth = treeAccount.getCanopyDepth() || 0 // default to 0 if undefined.
 
-    // parse the list of proof addresses into a valid AccountMeta[]
+    // Parse the list of proof addresses into a valid AccountMeta[].
     const proofPath: AccountMeta[] = result.proof
       .map((node: string) => ({
         pubkey: new PublicKey(node),
         isSigner: false,
         isWritable: false,
       }))
-      .slice(0, result.proof.length - (!!canopyDepth ? canopyDepth : 0))
+      .slice(0, result.proof.length - canopyDepth)
 
+    // Create the transfer instruction.
     const transferIx = createTransferInstruction(
       {
-        merkleTree: treeKeypair.publicKey,
+        merkleTree: treePublicKey,
         treeAuthority,
         leafOwner,
         leafDelegate,
-        newLeafOwner,
+        newLeafOwner: payer.publicKey, // transfer to self for testing
         logWrapper: SPL_NOOP_PROGRAM_ID,
         compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
         anchorRemainingAccounts: proofPath,
       },
       {
         root: [...new PublicKey(result.root.trim()).toBytes()],
-        dataHash: [
-          ...new PublicKey(cnft.compression.data_hash.trim()).toBytes(),
-        ],
+        dataHash: [...new PublicKey(compression.data_hash.trim()).toBytes()],
         creatorHash: [
-          ...new PublicKey(cnft.compression.creator_hash.trim()).toBytes(),
+          ...new PublicKey(compression.creator_hash.trim()).toBytes(),
         ],
-        nonce: cnft.compression.leaf_id,
-        index: cnft.compression.leaf_id,
+        nonce: compression.leaf_id,
+        index: compression.leaf_id,
       },
       BUBBLEGUM_PROGRAM_ID
     )
@@ -248,231 +305,24 @@ describe("Test", () => {
     try {
       // create and send the transaction to transfer ownership of the NFT
       const tx = new Transaction().add(transferIx)
-      tx.feePayer = user.publicKey
+      tx.feePayer = payer.publicKey
 
-      // send the transaction
       const txSignature = await sendAndConfirmTransaction(
         connection,
         tx,
-        // ensuring the feePayer signs the transaction
-        [user],
+        [payer],
         {
           commitment: "confirmed",
           skipPreflight: true,
         }
       )
 
-      console.log("\nTransfer successful!\n", explorerURL({ txSignature }))
+      console.log(
+        `https://explorer.solana.com/tx/${txSignature}?cluster=devnet`
+      )
     } catch (err: any) {
-      console.error("\nFailed to create transfer nft:", err)
-
-      console.log("\n=======================")
-      console.log("  Transfer failed!")
-      console.log("=======================")
-
-      // log a block explorer link for the failed transaction
-
+      console.error("\nFailed to transfer nft:", err)
       throw err
     }
   })
 })
-
-async function createTree(
-  connection: Connection,
-  payer: Keypair,
-  treeKeypair: Keypair,
-  maxDepthSizePair: ValidDepthSizePair,
-  canopyDepth: number = 0
-) {
-  console.log("Creating a new Merkle tree...")
-  console.log("treeAddress:", treeKeypair.publicKey.toBase58())
-
-  // derive the tree's authority (PDA), owned by Bubblegum
-  const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
-    [treeKeypair.publicKey.toBuffer()],
-    BUBBLEGUM_PROGRAM_ID
-  )
-
-  console.log("treeAuthority:", treeAuthority.toBase58())
-
-  // allocate the tree's account on chain with the `space`
-  // NOTE: this will compute the space needed to store the tree on chain (and the lamports required to store it)
-  const allocTreeIx = await createAllocTreeIx(
-    connection,
-    treeKeypair.publicKey,
-    payer.publicKey,
-    maxDepthSizePair,
-    canopyDepth
-  )
-
-  // create the instruction to actually create the tree
-  const createTreeIx = createCreateTreeInstruction(
-    {
-      payer: payer.publicKey,
-      treeCreator: payer.publicKey,
-      treeAuthority,
-      merkleTree: treeKeypair.publicKey,
-      compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-      // NOTE: this is used for some on chain logging
-      logWrapper: SPL_NOOP_PROGRAM_ID,
-    },
-    {
-      maxBufferSize: maxDepthSizePair.maxBufferSize,
-      maxDepth: maxDepthSizePair.maxDepth,
-      public: false,
-    },
-    BUBBLEGUM_PROGRAM_ID
-  )
-
-  try {
-    // create and send the transaction to initialize the tree
-    const tx = new Transaction().add(allocTreeIx, createTreeIx)
-    tx.feePayer = payer.publicKey
-
-    // send the transaction
-    const txSignature = await sendAndConfirmTransaction(
-      connection,
-      tx,
-      // ensuring the `treeKeypair` PDA and the `payer` are BOTH signers
-      [treeKeypair, payer],
-      {
-        commitment: "confirmed",
-        skipPreflight: true,
-      }
-    )
-
-    console.log("\nMerkle tree created successfully!")
-    console.log(explorerURL({ txSignature }))
-
-    // return useful info
-    return { treeAuthority, treeAddress: treeKeypair.publicKey }
-  } catch (err: any) {
-    console.error("\nFailed to create merkle tree:", err)
-
-    throw err
-  }
-}
-
-async function mintCompressedNFT(
-  connection: Connection,
-  payer: Keypair,
-  treeAddress: PublicKey,
-  collectionMint: PublicKey,
-  collectionMetadata: PublicKey,
-  collectionMasterEditionAccount: PublicKey,
-  compressedNFTMetadata: MetadataArgs,
-  receiverAddress?: PublicKey
-) {
-  // derive the tree's authority (PDA), owned by Bubblegum
-  const [treeAuthority, _bump] = PublicKey.findProgramAddressSync(
-    [treeAddress.toBuffer()],
-    BUBBLEGUM_PROGRAM_ID
-  )
-
-  // derive a PDA (owned by Bubblegum) to act as the signer of the compressed minting
-  const [bubblegumSigner, _bump2] = PublicKey.findProgramAddressSync(
-    // `collection_cpi` is a custom prefix required by the Bubblegum program
-    [Buffer.from("collection_cpi", "utf8")],
-    BUBBLEGUM_PROGRAM_ID
-  )
-
-  // create an array of instruction, to mint multiple compressed NFTs at once
-  const mintIxs: TransactionInstruction[] = []
-
-  /*
-    Add a single mint instruction
-    ---
-    But you could all multiple in the same transaction, as long as your
-    transaction is still within the byte size limits
-  */
-  mintIxs.push(
-    createMintToCollectionV1Instruction(
-      {
-        payer: payer.publicKey,
-
-        merkleTree: treeAddress,
-        treeAuthority,
-        treeDelegate: payer.publicKey,
-
-        // set the receiver of the NFT
-        leafOwner: receiverAddress || payer.publicKey,
-        // set a delegated authority over this NFT
-        leafDelegate: payer.publicKey,
-
-        /*
-            You can set any delegate address at mint, otherwise should
-            normally be the same as `leafOwner`
-            NOTE: the delegate will be auto cleared upon NFT transfer
-            ---
-            in this case, we are setting the payer as the delegate
-          */
-
-        // collection details
-        collectionAuthority: payer.publicKey,
-        collectionAuthorityRecordPda: BUBBLEGUM_PROGRAM_ID,
-        collectionMint: collectionMint,
-        collectionMetadata: collectionMetadata,
-        editionAccount: collectionMasterEditionAccount,
-
-        // other accounts
-        compressionProgram: SPL_ACCOUNT_COMPRESSION_PROGRAM_ID,
-        logWrapper: SPL_NOOP_PROGRAM_ID,
-        bubblegumSigner: bubblegumSigner,
-        tokenMetadataProgram: TOKEN_METADATA_PROGRAM_ID,
-      },
-      {
-        metadataArgs: Object.assign(compressedNFTMetadata, {
-          collection: { key: collectionMint, verified: false },
-        }),
-      }
-    )
-  )
-
-  try {
-    // construct the transaction with our instructions, making the `payer` the `feePayer`
-    const tx = new Transaction().add(...mintIxs)
-    tx.feePayer = payer.publicKey
-
-    // send the transaction to the cluster
-    const txSignature = await sendAndConfirmTransaction(
-      connection,
-      tx,
-      [payer],
-      {
-        commitment: "confirmed",
-        skipPreflight: true,
-      }
-    )
-
-    console.log("\nSuccessfully minted the compressed NFT!")
-    console.log(explorerURL({ txSignature }))
-
-    return txSignature
-  } catch (err) {
-    console.error("\nFailed to mint compressed NFT:", err)
-
-    throw err
-  }
-}
-
-function explorerURL({
-  address,
-  txSignature,
-  cluster,
-}: {
-  address?: string
-  txSignature?: string
-  cluster?: "devnet" | "testnet" | "mainnet" | "mainnet-beta"
-}) {
-  let baseUrl: string
-  //
-  if (address) baseUrl = `https://explorer.solana.com/address/${address}`
-  else if (txSignature)
-    baseUrl = `https://explorer.solana.com/tx/${txSignature}`
-  else return "[unknown]"
-
-  // auto append the desired search params
-  const url = new URL(baseUrl)
-  url.searchParams.append("cluster", cluster || "devnet")
-  return url.toString() + "\n"
-}
